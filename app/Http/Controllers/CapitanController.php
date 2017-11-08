8ba947c64858fb2503a6c8abfd7cc7a96f89edbe
Gabriel Falcones <gafalcon@espol.edu.ec>
2017-11-07 23:05:08 -0500

    created initial scaffoldings

---
+++ app/Http/Controllers/CapitanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Capitan;
+use Illuminate\Http\Request;
+
+class CapitanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Capitan  $capitan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Capitan $capitan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Capitan  $capitan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Capitan $capitan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Capitan  $capitan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Capitan $capitan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Capitan  $capitan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Capitan $capitan)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/EmbarcacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Embarcacion;
+use Illuminate\Http\Request;
+
+class EmbarcacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Embarcacion  $embarcacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Embarcacion $embarcacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Embarcacion  $embarcacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Embarcacion $embarcacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Embarcacion  $embarcacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Embarcacion $embarcacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Embarcacion  $embarcacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Embarcacion $embarcacion)
+    {
+        //
+    }
+}
